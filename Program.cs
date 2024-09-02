25ce49e2222c8398eefa225740c68ac037615f3e
maribvidal <sdsdsd8097@gmail.com>
2024-09-02 15:58:44 -0300

    Se añadió la función de comprimir también las carpetas presentes en el directorio. Además se modificó ligeramente la presentación del programa y el catcheo de errores.

--- Program.cs
+++ Program.cs
@@ -7,70 +7,113 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        bool runtime = true;
         string zipName = "zip";
+
         List<string> totalFilePaths;
+        List<string> totalDirPaths;
         List<string> selectedFilePaths;
+        List<string> selectedDirPaths;
 
-        Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
-
-        try
+        // While the program is running...
+        while (runtime) 
         {
-            // Set new working directory if an argument was passed
-            if (args.Length > 0)
-            {
-                if (!Directory.Exists(args[0]))
-                    throw new InvalidOperationException("That directory doesn't exist");
+            Console.Clear();
+            Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
 
-                Directory.SetCurrentDirectory(args[0]);
-
-                if (args.Length > 1)
-                    zipName = args[1];
-            }
-            else
+            try
             {
-                Console.WriteLine(" * If you're not providing any arguments, current directory will be used");
-                Console.Write(" * Is this what you want? (Y/N): ");
-                if (UserInput() == "N")
+                // Set new working directory if an argument was passed
+                if (args.Length > 0)
                 {
-                    Console.WriteLine(" * Enter a valid directory path: ");
-                    string dirPath = UserInput();
-                    while (!Directory.Exists(dirPath))
-                        dirPath = UserInput();
-                    Directory.SetCurrentDirectory(dirPath);    
+                    if (!Directory.Exists(args[0]))
+                        throw new InvalidOperationException("That directory doesn't exist");
+
+                    Directory.SetCurrentDirectory(args[0]);
+
+                    if (args.Length > 1)
+                        zipName = args[1];
+                }
+                else
+                {
+                    Console.WriteLine(" * If you're not providing any arguments, current directory will be used");
+                    Console.WriteLine(" * Is this what you want? (Y/N) ");
+                    if (UserInput() == "N")
+                    {
+                        Console.WriteLine(" * Enter a valid directory path: ");
+                        string dirPath = UserInput();
+                        while (!Directory.Exists(dirPath))
+                            dirPath = UserInput();
+                        Directory.SetCurrentDirectory(dirPath);    
+                    }
+                }
+
+                Console.Clear();
+                Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
+                Console.WriteLine(" -- SELECT THE FILES YOU WANT TO COMPRESS -- ");
+
+                // Enumerate directory files
+                totalFilePaths = Directory.EnumerateFiles(Directory.GetCurrentDirectory()).ToList();
+                selectedFilePaths = SelectFilePaths(totalFilePaths);
+
+                // Enumerate directory paths
+                totalDirPaths = Directory.EnumerateDirectories(Directory.GetCurrentDirectory()).ToList();
+                selectedDirPaths = SelectDirPaths(totalDirPaths);
+
+                Console.Clear();
+                Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
+                Console.WriteLine(" -- NOW CREATING ZIP -- ");
+
+                ZipManager zipManager = new ZipManager();
+                zipManager.CreateZip(selectedFilePaths, selectedDirPaths, zipName);
+
+                Console.Clear();
+                Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
+                Console.WriteLine(" -- ZIP CREATED SUCCESFULLY -- ");
+                Console.WriteLine(" * You can close the program");
+                runtime = false;
+
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Clear();
+                Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
+                Console.WriteLine(" -- ERROR -- ");
+                Console.WriteLine($" * One path is at least required to create a .zip");
+                Console.WriteLine(" * \n * Do you wish to stop the program? (Y/N)");
+
+                if (UserInput() == "Y") 
+                {
+                    runtime = false;
+                    Console.WriteLine(" * Closing app");
                 }
             }
-
-            Console.Clear();
-            Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
-            Console.WriteLine(" -- SELECT THE FILES YOU WANT TO COMPRESS -- ");
-
-            // Enumerate directory files
-            totalFilePaths = Directory.EnumerateFiles(Directory.GetCurrentDirectory()).ToList();
-            selectedFilePaths = SelectPaths(totalFilePaths);
-
-            Console.Clear();
-            Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
-            Console.WriteLine(" -- NOW CREATING ZIP -- ");
-
-            ZipManager zipManager = new ZipManager();
-            zipManager.CreateZip(selectedFilePaths, zipName);
-
-            Console.Clear();
-            Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
-            Console.WriteLine(" -- ZIP CREATED SUCCESFULLY -- ");
-            Console.WriteLine(" * You can close the program");
-
-        }
-        catch (Exception ex)
-        {
-            Console.Clear();
-            Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
-            Console.WriteLine(" -- ERROR -- ");
-            Console.WriteLine($" * {ex.Message}");
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine(" == ZIPFILE MANAGER (v0.1) == ");
+                Console.WriteLine(" -- ERROR -- ");
+                Console.WriteLine($" * {ex.Message}");
+                runtime = false;
+            }
         }
     }
 
-    private static List<string> SelectPaths(List<string> paths)
+    private static List<string> SelectFilePaths(List<string> paths)
+    {
+        List<string> newPaths = new List<string>();
+
+        foreach (string path in paths)
+        {
+            Console.Write($" // {path} / ADD? (Y/N): ");
+            if (UserInput() == "Y")
+                newPaths.Add(path);
+        }
+
+        return newPaths;
+    }
+
+    private static List<string> SelectDirPaths(List<string> paths)
     {
         List<string> newPaths = new List<string>();
 
@@ -86,6 +129,7 @@ public class Program
 
     private static string UserInput()
     {
+        Console.Write(" > ");
         string? input = Console.ReadLine();
         while (String.IsNullOrEmpty(input))
             input = Console.ReadLine();
--- ZipManager.cs
+++ ZipManager.cs
@@ -10,20 +10,24 @@ public class ZipManager
 {
     // MÉTODO: CreateZip
     // - Parámetros: fileList (recibe una lista de strings que representan las rutas a los archivos)
-    public void CreateZip(List<string> fileList, string zipName = "zip")
+    public void CreateZip(List<string> fileList, List<string> dirList, string zipName = "zip")
     {
         // Troubleshooting
-        List<string> validPaths;
+        List<string> validFilePaths;
+        List<string> validDirPaths;
         int invalidPaths = 0;
 
-        if (fileList.Count < 1)
+        if (fileList.Count < 1 && dirList.Count < 1)
             throw new ArgumentOutOfRangeException("At least one path is needed");
         
-        // Check every path
-        validPaths = CheckPaths(fileList, ref invalidPaths);
+        // Check every file path
+        validFilePaths = CheckFilePaths(fileList, ref invalidPaths);
+
+        // Check every directory path
+        validDirPaths = CheckDirPaths(dirList, ref invalidPaths);
 
         // Create the .zip
-        if (validPaths.Count < 1)
+        if (validFilePaths.Count < 1 && validDirPaths.Count < 1)
             throw new InvalidOperationException("No valid path was received");
 
         zipName = (zipName == "zip") ? ("zip_generated_" + DateTime.UtcNow.Second + DateTime.UtcNow.Minute + DateTime.UtcNow.Hour + DateTime.UtcNow.Day) : zipName;
@@ -31,10 +35,20 @@ public class ZipManager
         {
             using (ZipArchive archive = new ZipArchive(zipFile, ZipArchiveMode.Update))
             {
-                foreach (string path in validPaths)
+                // First, the files
+                foreach (string path in validFilePaths)
                 {
                     var fileInfo = new FileInfo(path);
                     archive.CreateEntryFromFile(fileInfo.FullName,  fileInfo.Name);
+                }
+                // Then, the directories
+                foreach (string path in validDirPaths)
+                {
+                    var dirInfo = new DirectoryInfo(path);
+                    var dirFiles = dirInfo.GetFiles();
+                    
+                    foreach (FileInfo file in dirFiles)
+                        archive.CreateEntryFromFile(file.FullName, dirInfo.Name + "/" + file.Name);
                 }
             }
         }
@@ -42,19 +56,37 @@ public class ZipManager
         Debug.WriteLine($" -- COMPRESSION FINISHED: {invalidPaths} files weren't added");
     }
 
-    private List<string> CheckPaths(List<string> fileList, ref int invalidPaths)
+    private List<string> CheckFilePaths(List<string> fileList, ref int invalidPaths)
     {
-        List<string> validPaths = new List<string>();
+        List<string> validFilePaths = new List<string>();
+
         foreach (string path in fileList)
         {
             if (!String.IsNullOrEmpty(path))
             {
                 if (File.Exists(path))
-                    validPaths.Add(path);
+                    validFilePaths.Add(path);
             }
             else
                 invalidPaths++;
         }
-        return validPaths;
+        return validFilePaths;
+    }
+
+    private List<string> CheckDirPaths(List<string> dirList, ref int invalidPaths)
+    {
+        List<string> validDirPaths = new List<string>();
+
+        foreach (string path in dirList)
+        {
+            if (!String.IsNullOrEmpty(path))
+            {
+                if (Directory.Exists(path))
+                    validDirPaths.Add(path);
+            }
+            else
+                invalidPaths++;
+        }
+        return validDirPaths;
     }
 }
